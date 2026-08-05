bf0399eb2d399a0fb063262401eab1ebac0d5b48
calmespectre <gerrad.meitameisaruni@gmail.com>
2026-08-05 15:59:11 +0300

    added requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -106,6 +106,11 @@ CORS_ALLOWED_ORIGINS = [
     "https://child-tracking-system-backend.onrender.com",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://child-tracking-system-backend.onrender.com',
+    'https://child-tracking-system-pi.vercel.app',
+]
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
