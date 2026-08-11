4d801a9d32dd0b9a419379876f9fa9ddf74a9947
calmespectre <gerrad.meitameisaruni@gmail.com>
2026-08-11 13:11:18 +0300

    benifits log

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path(
-        "api/auth/",
-        include("authentication.urls"),
-    ),
-
-    path(
-        "api/beneficiaries/",
-        include("beneficiaries.urls"),
-    ),
+    path("api/auth/", include("accounts.urls")),
+    path("api/beneficiaries/", include("beneficiaries.urls")),
 ]
