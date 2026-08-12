239aae34b4836601ca0d74dbfcbe4546cf645af0
calmespectre <gerrad.meitameisaruni@gmail.com>
2026-08-12 13:54:46 +0300

    fixed beneficiary and support log API routes

--- config/urls.py
+++ config/urls.py
@@ -1,33 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 
-from beneficiaries.views import (
-    SupportLogViewSet,
-)
-
-support_router = DefaultRouter()
-support_router.register(
-    r"support-logs",
-    SupportLogViewSet,
-    basename="root-support-log",
-)
 
 urlpatterns = [
-    path(
-        "admin/",
-        admin.site.urls,
-    ),
-    path(
-        "api/auth/",
-        include("accounts.urls"),
-    ),
-    path(
-        "api/beneficiaries/",
-        include("beneficiaries.urls"),
-    ),
-    path(
-        "api/",
-        include(support_router.urls),
-    ),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("accounts.urls")),
+    path("api/beneficiaries/", include("beneficiaries.urls")),
 ]
