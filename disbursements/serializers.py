8671eb5c56d11cc785f9d8fcdb0223bc6971a56a
gerrad meitamei <gerrad.meitameisaruni@gmail.com>
2026-08-25 09:57:36 +0300

    fixed foreign null key

--- disbursements/serializers.py
+++ disbursements/serializers.py
@@ -3,14 +3,8 @@ from .models import Bursary
 
 
 class BursarySerializer(serializers.ModelSerializer):
-    beneficiary_id = serializers.IntegerField(
-        source="beneficiary.id",
-        read_only=True
-    )
-    beneficiary_child_number = serializers.CharField(
-        source="beneficiary.child_number",
-        read_only=True
-    )
+    beneficiary_id = serializers.SerializerMethodField()
+    beneficiary_child_number = serializers.SerializerMethodField()
 
     class Meta:
         model = Bursary
@@ -43,3 +37,9 @@ class BursarySerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
+
+    def get_beneficiary_id(self, obj):
+        return obj.beneficiary.id if obj.beneficiary else None
+
+    def get_beneficiary_child_number(self, obj):
+        return obj.beneficiary.child_number if obj.beneficiary else None
