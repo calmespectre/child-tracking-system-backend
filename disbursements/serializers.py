e171b555bfa5aa4ac6c7b021099ef28851c1d778
gerrad meitamei <gerrad.meitameisaruni@gmail.com>
2026-08-25 10:11:15 +0300

    fixed foreign null key

--- disbursements/serializers.py
+++ disbursements/serializers.py
@@ -23,8 +23,8 @@ class BursarySerializer(serializers.ModelSerializer):
             "date",
             "status",
             "notes",
-            "beneficiary_id",              # now a method field
-            "beneficiary_child_number",    # now a method field
+            "beneficiary_id",
+            "beneficiary_child_number",
             "created_by",
             "created_at",
             "updated_at",
